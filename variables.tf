fdfd918b93de4beb79b69c8bf463bb97c94448f3
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-19 15:23:15 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
-variable "yc_token" {}
-variable "yc_cloud_id" {}
-variable "yc_folder_id" {}
+variable "yc_token" {
+  description = "IAM token"
+  type        = string
+}
+
+variable "yc_cloud_id" {
+  description = "Cloud ID"
+  type        = string
+}
+
+variable "yc_folder_id" {
+  description = "Folder ID"
+  type        = string
+}
