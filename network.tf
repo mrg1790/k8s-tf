04814e974beb0a91797292f495c0f8c8e4c14184
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-19 20:47:52 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,10 +1,20 @@
-resource "yandex_vpc_network" "k8s" {
-  name = "k8s-network"
-}
-
-resource "yandex_vpc_subnet" "k8s-subnet" {
-  name           = "k8s-subnet"
+resource "yandex_vpc_subnet" "subnet-a" {
+  name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.k8s.id
-  v4_cidr_blocks = ["10.2.0.0/16"]
+  v4_cidr_blocks = ["10.2.1.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-b" {
+  name           = "subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.k8s.id
+  v4_cidr_blocks = ["10.2.2.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-d" {
+  name           = "subnet-c"
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.k8s.id
+  v4_cidr_blocks = ["10.2.3.0/24"]
 }
