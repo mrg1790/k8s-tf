cfab5d751a019a320871e0354984c12e3802d6b6
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-19 20:55:42 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "kubernetes_cluster_id" {
-  value = module.k8s.id
+  value = module.k8s.cluster_id
 }
 
-#output "kubernetes_cluster_name" {
-  #value = module.k8s.k8s_name
-#}
+output "kubernetes_cluster_name" {
+  value = module.k8s.cluster_name
+}
