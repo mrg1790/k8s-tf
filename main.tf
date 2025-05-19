57d5fb4116dba0cdf896d5d4cdb6ee0e91dfdd54
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-19 20:26:38 +0300

    main.tf updated

--- main.tf
+++ main.tf
@@ -14,24 +14,55 @@ provider "yandex" {
 }
 
 module "k8s" {
-  source  = "github.com/terraform-yc-modules/terraform-yc-kubernetes"
+  source = "github.com/terraform-yc-modules/terraform-yc-kubernetes"
 
-  cluster_name        = "my-cluster"
-  network_id          = yandex_vpc_network.k8s.id
-  cluster_zones       = ["ru-central1-a"]
-  service_account_id  = yandex_iam_service_account.k8s.id
+  # Конфигурация кластера
+  cluster_config = {
+    name               = "plafac"
+    network_id         = yandex_vpc_network.k8s.id
+    service_account_id = yandex_iam_service_account.k8s.id
 
+    # Для zonal-кластера — одна локация
+    master_locations = [
+      { zone = "ru-central1-a", subnet_id = yandex_vpc_subnet.subnet-a.id },
+      #{ zone = "ru-central1-b", subnet_id = yandex_vpc_subnet.subnet-b.id },
+      #{ zone = "ru-central1-c", subnet_id = yandex_vpc_subnet.subnet-c.id },
+    ]
+
+    # (опционально) диапазоны IP для кластера
+    # cluster_ipv4_range = "10.128.0.0/16"
+    # service_ipv4_range = "10.96.0.0/16"
+  }
+
+  # Группы нод
   node_groups = {
-    "default" = {
-      node_zone        = "ru-central1-a"
-      node_count       = 2
-      auto_scale       = false
-      auto_scale_min   = 2
-      auto_scale_max   = 5
-      node_labels      = { "role" = "worker" }
-      preemptible      = false
-      nat              = true
-      node_disk_size   = 50
+    workers = {
+      auto_scale   = false
+      node_count   = 1
+      cores        = 1
+      memory       = 2
+      node_disk_size = 20
+      nat          = true
+      # subnet_id не нужен — будет взят из master_locations
+    }
+    monitoring = {
+      auto_scale   = false
+      node_count   = 1
+      cores        = 1
+      memory       = 2
+      node_disk_size = 20
+      nat          = true
+      # subnet_id не нужен — будет взят из master_locations
+    }
+    postgres = {
+      auto_scale   = false
+      node_count   = 1
+      cores        = 1
+      memory       = 2
+      node_disk_size = 20
+      nat          = true
+      # subnet_id не нужен — будет взят из master_locations
     }
   }
 }
+
