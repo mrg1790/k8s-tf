7a4d0e466c8cbeec669cfe473cf56b8079234c9b
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-20 12:07:59 +0300

    Update postgresql.tf

--- postgresql.tf
+++ postgresql.tf
@@ -1,9 +1,3 @@
-resource "kubernetes_namespace" "postgres" {
-  metadata {
-    name = "postgres"
-  }
-}
-
 resource "helm_release" "postgresql" {
   name       = "pg"
   namespace  = kubernetes_namespace.postgres.metadata[0].name
