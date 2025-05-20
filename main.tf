6d26400985e91c5940fc1b92138bae35806b0e8f
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-20 15:58:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,6 @@ terraform {
   }
   required_version = ">= 0.13"
 }
-provider "yandex" {
-  #token     = var.yc_token
-  #cloud_id  = var.yc_cloud_id
-  #folder_id = var.yc_folder_id
-  zone      = "ru-central1-a"
-}
 
 module "k8s" {
   source = "github.com/terraform-yc-modules/terraform-yc-kubernetes"
