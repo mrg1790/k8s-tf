1370d54ecc5950af1d063cdd2cd72b5c6eac3786
mrg1790 <127439857+mrg1790@users.noreply.github.com>
2025-05-20 15:58:43 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,17 @@
+provider "yandex" {
+  #token     = var.yc_token
+  #cloud_id  = var.yc_cloud_id
+  #folder_id = var.yc_folder_id
+  zone      = "ru-central1-a"
+}
 provider "kubernetes" {
   config_path = "~/.kube/config"
   #config_context = "yc-k8s-cluster-jmhhsgnq"
+}
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    #config_context = "yc-k8s-cluster-jmhhsgnq"
+    load_config_file = false      # чтобы не лезть в ~/.kube/config дважды
+  }
 }
